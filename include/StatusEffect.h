d55ef4b1b7b0fc5ba10fe87a2adca7ed416800aa
Søren R.B <soer652c@gmail.com>
2026-06-14 13:11:29 +0200

    Add status effect class

---
+++ include/StatusEffect.h
@@ -0,0 +1,37 @@
+#ifndef STATUS_EFFECT_H
+#define STATUS_EFFECT_H
+
+#include <iosfwd>
+#include <random>
+#include <string>
+
+class Monster;
+
+enum class StatusType {
+    Poisoned,
+    Frozen,
+    Paralyzed,
+    Stunned,
+    Cursed,
+    Focused
+};
+
+class StatusEffect {
+public:
+    StatusEffect(StatusType type, int remainingTurns);
+
+    StatusType type() const;
+    const std::string& name() const;
+    int remainingTurns() const;
+    bool isExpired() const;
+
+    bool applyStartOfTurn(Monster& monster, std::ostream& output, std::mt19937& randomEngine);
+    void applyAfterDealingDamage(Monster& monster, std::ostream& output) const;
+
+private:
+    StatusType type_;
+    int remainingTurns_;
+    std::string name_;
+};
+
+#endif
---
+++ src/StatusEffect.cpp
@@ -0,0 +1,95 @@
+#include "StatusEffect.h"
+
+#include "Monster.h"
+
+#include <algorithm>
+#include <iostream>
+
+namespace {
+std::string statusName(StatusType type) {
+    switch (type) {
+    case StatusType::Poisoned:
+        return "Poisoned";
+    case StatusType::Frozen:
+        return "Frozen";
+    case StatusType::Paralyzed:
+        return "Paralyzed";
+    case StatusType::Stunned:
+        return "Stunned";
+    case StatusType::Cursed:
+        return "Cursed";
+    case StatusType::Focused:
+        return "Focused";
+    }
+
+    return "Unknown";
+}
+}
+
+StatusEffect::StatusEffect(StatusType type, int remainingTurns)
+    : type_(type),
+      remainingTurns_(std::max(1, remainingTurns)),
+      name_(statusName(type)) {}
+
+StatusType StatusEffect::type() const {
+    return type_;
+}
+
+const std::string& StatusEffect::name() const {
+    return name_;
+}
+
+int StatusEffect::remainingTurns() const {
+    return remainingTurns_;
+}
+
+bool StatusEffect::isExpired() const {
+    return remainingTurns_ <= 0;
+}
+
+bool StatusEffect::applyStartOfTurn(
+    Monster& monster, std::ostream& output, std::mt19937& randomEngine) {
+    bool canAct = true;
+
+    switch (type_) {
+    case StatusType::Poisoned:
+        monster.takeDamage(2);
+        output << monster.name() << " tager 2 skade af Poisoned.\n";
+        break;
+    case StatusType::Frozen:
+        output << monster.name() << " er Frozen og kan ikke handle.\n";
+        canAct = false;
+        break;
+    case StatusType::Paralyzed: {
+        std::bernoulli_distribution losesTurn(0.30);
+        if (losesTurn(randomEngine)) {
+            output << monster.name() << " er Paralyzed og mister sin tur.\n";
+            canAct = false;
+        }
+        break;
+    }
+    case StatusType::Stunned:
+        output << monster.name() << " er Stunned og mister sin tur.\n";
+        canAct = false;
+        break;
+    case StatusType::Cursed:
+        output << monster.name() << " er Cursed.\n";
+        break;
+    case StatusType::Focused:
+        monster.heal(2);
+        output << monster.name() << " er Focused og heler 2 hp.\n";
+        break;
+    }
+
+    --remainingTurns_;
+    return canAct;
+}
+
+void StatusEffect::applyAfterDealingDamage(Monster& monster, std::ostream& output) const {
+    if (type_ != StatusType::Cursed) {
+        return;
+    }
+
+    monster.takeDamage(2);
+    output << monster.name() << " tager 2 skade af Cursed efter sit angreb.\n";
+}
